0664c1d206b4b3cc6069bfecdaf42ff4044dbcbb
Nandu krishna <nandukrishna5745@gmail.com>
2026-05-06 10:30:41 +0530

    fixeddd

--- src/App.jsx
+++ src/App.jsx
@@ -322,7 +322,7 @@ function App() {
 
       <Analytics />
 
-      {/* Floating Glass Bottom Nav */}
+      {}
       <nav className="mobile-bottom-nav">
         <div className={`mobile-nav-item ${page === "home" ? "active" : ""}`} onClick={() => handlePageChange("home")}>
           <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
